16fd14e0127f80f05dcc28d86eb4c62dce80fbd5
abhay0826R-dev <abhay0826r@gmail.com>
2026-07-29 21:33:12 +0530

    feat: add user retrieval endpoint with pagination support

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,5 +1,7 @@
 from typing import Any
 from fastapi import APIRouter, HTTPException
+from sqlmodel import func, select
+
 from app import crud
 from app.api.deps import (
     SessionDep,
@@ -8,10 +10,25 @@ from app.models.model import (
     UserCreate,
     UserPublic,
     UserRegister,
+    User,
+    UsersPublic,
 )
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+# GET ALL USERS
+@router.get("/", response_model=UsersPublic)
+def get_users(session: SessionDep, skip: int = 0, limit: int = 50) -> Any:
+    count_statement = select(func.count()).select_from(User)
+    count = session.exec(count_statement).one()
+
+    # sorting
+    statement = select(User).offset(skip).limit(limit)
+    users = session.exec(statement).all()
+
+    return UsersPublic(data=users, count=count)
+
+# CREATE NEW USERs
 @router.post("/signup", response_model=UserPublic)
 def register_user(session: SessionDep, user_in: UserRegister) -> Any :
     user = crud.get_user_by_email(session=session, email=user_in.email)
