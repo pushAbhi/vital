be7ca59a624b9399f8a3692e04ba528d82ea7eff
abhay0826R-dev <abhay0826r@gmail.com>
2026-08-01 00:16:56 +0530

    feat/fix: add user update functionality and fix typo fullname to full_name

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -5,7 +5,8 @@ from sqlmodel import func, select
 from app import crud
 from app.api.deps import (
     SessionDep,
-    get_current_active_superuser
+    get_current_active_superuser,
+    CurrentUser
 )
 from app.models.model import (
     UserCreate,
@@ -13,6 +14,7 @@ from app.models.model import (
     UserRegister,
     User,
     UsersPublic,
+    UserUpdateMe
 )
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -41,3 +43,19 @@ def register_user(session: SessionDep, user_in: UserRegister) -> Any :
     user_create = UserCreate.model_validate(user_in)
     user = crud.create_user(session=session, user_create=user_create)
     return user
+
+@router.patch("/me", response_model=UserPublic)
+def update_user_me(*, session: SessionDep, user_in: UserUpdateMe, current_user: CurrentUser) -> Any:
+    if user_in.email:
+        existing_user = crud.get_user_by_email(session=session, email=user_in.email)
+        if existing_user and existing_user.id != current_user.id:
+            raise HTTPException(
+                status_code=409,
+                detail="User with this email already exists"
+            )
+    user_data = user_in.model_dump(exclude_unset=True)
+    current_user.sqlmodel_update(user_data)
+    session.add(current_user)
+    session.commit()
+    session.refresh(current_user)
+    return current_user
